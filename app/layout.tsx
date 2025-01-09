969e56ef8faec36d0f7acc6b2588f6b7dded60bb
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 02:43:46 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,15 +57,15 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <title>Sqtoto : Togel Deposit Pulsa Tanpa Potongan Dan Daftar Togel Terpercaya</title>
-        <meta name="description" content="18toto hadir sebagai solusi bagi pecinta slot online yang mencari kemudahan dan kenyamanan dalam bermain. Dengan fitur slot deposit 3000 via Dana, Anda dapat menikmati permainan seru tanpa harus mengeluarkan modal besar" />
-        <meta name="keywords" content="18toto, Deposit Dana 5000k, Situs Judi Online, Dana Slot77, situs Slot Dana, Gopay 69 Slot, Situs Slot Deposit 5000, Situs Slot Gacor Hari Ini Via Dana, Slot Deposit 3000 Via Dana, Situs Slot Gacor Jam Ini" />
-        <meta name="google-site-verification" content="veMyOTuOGYmEQDMDDL9R58xwmmCMdesJMcTMkYDqZ3Q" />
-        <meta name="application-name" content="18toto" />
+        <meta name="description" content="Sqtoto adalah platform togel online yang menawarkan pengalaman bermain dengan kemudahan dan kenyamanan. Dengan sistem deposit pulsa tanpa potongan, pemain dapat melakukan transaksi dengan cepat dan tanpa biaya tambahan, menjadikannya pilihan ideal bagi para penggemar togel yang mencari cara praktis dan efisien untuk bermain" />
+        <meta name="keywords" content="Sqtoto, Togel Deposit Pulsa Tanpa Potongan, Daftar Togel Terpercaya, Situs Judi Online, Togel Online, Deposit Dana 5000, Slot Online, Agen Togel Online, Bandar Togel 77, Togel Deposit Pulsa 10RB Tanpa Potongan, Situs Togel Resmi Toto" />
+        <meta name="google-site-verification" content="nuajGrI2NNHzJ36PDMUxGf9FrLSe3sBXkghWB7o-nBY" />
+        <meta name="application-name" content="sqtoto" />
         <link rel="author" href={nameDomain} />
         <link rel="canonical" href={nameDomain} />
-        <meta name="author" content="18toto" />
-        <meta name="publisher" content="18toto" />
-        <meta name="copyright" content="18toto" />
+        <meta name="author" content="sqtoto" />
+        <meta name="publisher" content="sqtoto" />
+        <meta name="copyright" content="sqtoto" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
         <meta name="category" content="slot" />
@@ -82,14 +82,14 @@ export default function RootLayout({
         <meta property="og:title" content="Sqtoto : Togel Deposit Pulsa Tanpa Potongan Dan Daftar Togel Terpercaya" />
         <meta property="og:description" content="Sqtoto : Togel Deposit Pulsa Tanpa Potongan Dan Daftar Togel Terpercaya, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
         <meta property="og:url" content={nameDomain} />
-        <meta property="og:site_name" content="18toto" />
+        <meta property="og:site_name" content="sqtoto" />
         <meta property="og:locale" content="id_ID" />
         <meta property="og:image" content={nameDomain} />
         <meta property="og:image:width" content="840" />
         <meta property="og:image:height" content="480" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="18toto" />
+        <meta name="twitter:creator" content="sqtoto" />
         <meta name="twitter:title" content="Sqtoto : Togel Deposit Pulsa Tanpa Potongan Dan Daftar Togel Terpercaya" />
         <meta name="twitter:description" content="Sqtoto : Togel Deposit Pulsa Tanpa Potongan Dan Daftar Togel Terpercaya, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
         <meta name="twitter:image" content={nameDomain} />
